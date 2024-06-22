5c46b5b6969bcc3cdde4a6e6e06053a2a249ae9f
Muhammad Abu Huraira <abuhuraira2129@gmail.com>
2024-06-22 20:18:54 +0500

    JobLoader API Endpoint Fixed

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://react-jobs-json.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
